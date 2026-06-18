0caf4fce217d164e42a866f8a808e4edd5633f45
thepointer1982-maker <thepointer1982@gmail.com>
2026-06-18 05:33:15 +0200

    Fix application bootstrap imports and menu default

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -16,35 +16,34 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 declare(strict_types=1);
 
 namespace OCA\LogCleaner\AppInfo;
 
-use OCP\AppFramework\App;
+use OCA\LogCleaner\Dashboard\LogCleanerWidget;
+use OCA\LogCleaner\Dashboard\LogCleanerWidget2;
 use OCP\App\IAppManager;
+use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\IConfig;
 use OCP\INavigationManager;
 use OCP\IURLGenerator;
-use OCP\IConfig;
-//use OCP\IAppConfig;
-//use OCP\IServerContainer;
-use Psr\Container\ContainerInterface;
-use OCA\LogCleaner\Dashboard\LogCleanerWidget;
-use OCA\LogCleaner\Dashboard\LogCleanerWidget2;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Throwable;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'logcleaner';
-	
-	//public function __construct(private IAppConfig $appConfig,) {
+
 	public function __construct() {
 		parent::__construct(self::APP_ID);
 	}
@@ -55,7 +54,6 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function boot(IBootContext $context): void {
-		$server = $context->getServerContainer();
 		try {
 			$context->injectFn($this->registerAppsManagementNavigation(...));
 		} catch (NotFoundExceptionInterface|ContainerExceptionInterface|Throwable) {
@@ -64,16 +62,17 @@ class Application extends App implements IBootstrap {
 
 	private function registerAppsManagementNavigation(IAppManager $appManager): void {
 		$container = $this->getContainer();
-		//$this->config = $config;
-		$config = $this->getContainer()->query(IConfig::class);
-		$appManager->enableAppForGroups(self::APP_ID, array('admin'), false);
-		$wtpara_menue = (int)$config->getAppValue(self::APP_ID, 'wtparam_menue');
-		//$wtpara_menue = (int)$this->appConfig->getValueString('logcleaner', 'wtparam_menue', '1', false);
-		if (!isset($wtpara_menue)) {
+		$config = $container->query(IConfig::class);
+
+		$appManager->enableAppForGroups(self::APP_ID, ['admin'], false);
+
+		$wtpara_menue = (int)$config->getAppValue(self::APP_ID, 'wtparam_menue', '1');
+		if ($wtpara_menue < 1 || $wtpara_menue > 2) {
 			$wtpara_menue = 1;
-			$this->config->setAppValue(self::APP_ID, 'wtparam_menue', 1);
+			$config->setAppValue(self::APP_ID, 'wtparam_menue', '1');
 		}
-		if ($wtpara_menue == 1) { // right
+
+		if ($wtpara_menue === 1) {
 			$container->get(INavigationManager::class)->add(function () use ($container) {
 				$urlGenerator = $container->get(IURLGenerator::class);
 				return [
@@ -82,19 +81,18 @@ class Application extends App implements IBootstrap {
 					'href' => $urlGenerator->linkToRoute(self::APP_ID.'.page.index'),
 					'icon' => $urlGenerator->imagePath(self::APP_ID, self::APP_ID.'-dark.svg'),
 					'name' => 'LogCleaner',
-					'type' => 'settings'
+					'type' => 'settings',
 				];
 			});
-		}
-		else { // top
+		} else {
 			$container->get(INavigationManager::class)->add(function () use ($container) {
 				$urlGenerator = $container->get(IURLGenerator::class);
 				return [
-				'id' => self::APP_ID,
-				'order' => 1000,
-				'href' => $urlGenerator->linkToRoute(self::APP_ID.'.page.index'),
-				'icon' => $urlGenerator->imagePath(self::APP_ID, self::APP_ID.'.svg'),
-				'name' => 'LogCleaner',
+					'id' => self::APP_ID,
+					'order' => 1000,
+					'href' => $urlGenerator->linkToRoute(self::APP_ID.'.page.index'),
+					'icon' => $urlGenerator->imagePath(self::APP_ID, self::APP_ID.'.svg'),
+					'name' => 'LogCleaner',
 				];
 			});
 		}
